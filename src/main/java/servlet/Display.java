f9f5dc9cf89fa282df0e62613c3718f43c88fcb6
Andrea Capuano <andrealfie@gmail.com>
2015-02-20 13:52:30 +0100

    Added search options. Refactored methods

--- src/main/java/servlet/Display.java
+++ src/main/java/servlet/Display.java
@@ -39,39 +39,66 @@ public class Display extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Get the parameter we need. 
+		/* Getting the searched parameter */ 
 		String topic = request.getParameter("topic");
 		if(topic == null) {
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 			return;
 		}
 		
-		//Search
-		RestClient restClient = new HttpRestClient();
-		Submissions subms = new Submissions(restClient);
+		String etaParam = request.getParameter("eta");
+		String nsfwParam = request.getParameter("nsfw");
+		boolean nsfwAllowed = processNswfParam(nsfwParam);
+		TimeSpan eta = processEtaParam(etaParam);
 		
-		HashSet<Submission> toRemove = new HashSet<Submission>();
-		List<Submission> submissionsSubreddit = subms.search(topic + " self:no nsfw:no", QuerySyntax.LUCENE, SearchSort.HOT, TimeSpan.MONTH, -1, RESULTS_NUMBER, null, null, true); 
+		List<Submission> submissions = doSearch(topic,nsfwAllowed,eta,RESULTS_NUMBER);
 		
-		if(submissionsSubreddit.isEmpty()){
+		/* Checking if the given query returned empty */
+		if(submissions.isEmpty()){
 			request.setAttribute("errMessage", "No results were returned from your search");
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 			return;
 		}
-		//Filtering non-thumbnail results
-		for(int i = 0; i < submissionsSubreddit.size(); i++){
-			String thumb = submissionsSubreddit.get(i).getThumbnail();
-			if(thumb.isEmpty() || !hasImageExtension(thumb))
-				toRemove.add(submissionsSubreddit.get(i));
-		}
-		for(Submission s : toRemove){
-			submissionsSubreddit.remove(s);
-		}
 		
+		filterSubmissions(submissions);
 		
-		request.setAttribute("submissions", submissionsSubreddit);
+		request.setAttribute("submissions", submissions);
+		request.setAttribute("etaparam", etaParam);
+		request.setAttribute("topic", topic);
+		request.setAttribute("nsfwparam", nsfwParam);
 		request.getRequestDispatcher("display.jsp").forward(request, response);
 	}
+	
+	/**
+	 * Processes the eta param
+	 * @param etaParam
+	 * @return the time span aka when to search.
+	 */
+	private TimeSpan processEtaParam(String etaParam) {
+		if(etaParam != null){
+			if(etaParam.equalsIgnoreCase("hour")) return TimeSpan.HOUR;
+			if(etaParam.equalsIgnoreCase("day")) return TimeSpan.DAY;
+			if(etaParam.equalsIgnoreCase("month")) return TimeSpan.MONTH;
+			if(etaParam.equalsIgnoreCase("week")) return TimeSpan.WEEK;
+			if(etaParam.equalsIgnoreCase("year")) return TimeSpan.YEAR;
+			if(etaParam.equalsIgnoreCase("all")) return TimeSpan.ALL;
+		}
+		return TimeSpan.ALL;
+	}
+
+	/**
+	 * Processes the nsfw param
+	 * @param nsfwParam
+	 * @return true if nsfw is allowed
+	 */
+	private boolean processNswfParam(String nsfwParam) {
+		if(nsfwParam != null){
+			if(nsfwParam.equalsIgnoreCase("yes")) return true;
+		}
+		return false;
+	}
+	
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -80,9 +107,65 @@ public class Display extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 	
-	public boolean hasImageExtension(String imageString)
-	{
+	/**
+	 * Checks if a given string has an image extension.
+	 * @param imageString the String to be checked
+	 * @return true if it has an image extension
+	 */
+	private boolean hasImageExtension(String imageString){
 		return imageString.endsWith(".jpg") || imageString.endsWith(".gif") || imageString.endsWith(".png");
+	}
+	
+	/**
+	 * Removes the submissions without thumbnails.
+	 * @param submissions the fetched submissions.
+	 */
+	private List<Submission> filterSubmissions(List<Submission> submissionsSubreddit){
+
+		HashSet<Submission> toRemove = new HashSet<Submission>();
+		
+		/* Adding submission without thumbnail to the toRemove set*/
+		for(int i = 0; i < submissionsSubreddit.size(); i++){
+			Submission submission = submissionsSubreddit.get(i);
+			String thumb = submission.getThumbnail();
+			if(thumb.isEmpty() || !hasImageExtension(thumb)){
+				
+				if(submission.isNSFW())
+					submission.setThumbnail("http://i.imgur.com/UHzw6.png");
+				else
+					toRemove.add(submission);
+				
+			}
+		}
+		
+		/* Removing flagged submissions */
+		for(Submission s : toRemove){
+			submissionsSubreddit.remove(s);
+		}
+		
+		return submissionsSubreddit;
+	}
+	
+	/**
+	 * Performs the search on reddit.
+	 * @param topic the searched term.
+	 * @param nsfw not safe for work results allowed
+	 * @param time the timespan of the submmissions
+	 * @param numberOfResults the number of results
+	 * @return the submissions
+	 */
+	private List<Submission> doSearch(String topic,boolean nsfw,TimeSpan time,int numberOfResults){
+		/* Searching through Reddit using JReddit */
+		RestClient restClient = new HttpRestClient();
+		Submissions subms = new Submissions(restClient);
+		
+		if(nsfw)
+			topic += " nsfw:yes";
+		else
+			topic += " nsfw:no";
+		
+		List<Submission> submissionsSubreddit = subms.search(topic, QuerySyntax.LUCENE, SearchSort.HOT, time, -1, numberOfResults, null, null, true); 
+		return submissionsSubreddit;
 	}
 
 }
--- src/main/webapp/display.jsp
+++ src/main/webapp/display.jsp
@@ -1,9 +1,29 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <c:import url="header.jsp"></c:import>
 
 <form method="get" action="Display">
-<input style="margin: 0 auto" type="text" name="topic" placeholder="What do you want to read about?" />
+<input style="margin: 0 auto" value="${requestScope.topic}" type="text" name="topic" placeholder="What do you want to read about?" />
+<div id="selectables">
+<label>
+Timespan:
+	<select name="eta">
+		<option value="hour" <c:if test="${'hour' eq requestScope.etaparam}">selected</c:if>>Hour</option>
+		<option value="day" <c:if test="${'day' eq requestScope.etaparam}">selected</c:if>>Day</option>
+		<option value="week" <c:if test="${'week' eq requestScope.etaparam}">selected</c:if>>Week</option>
+		<option value="month" <c:if test="${'month' eq requestScope.etaparam}">selected</c:if>>Month</option>
+		<option value="year" <c:if test="${'year' eq requestScope.etaparam}">selected</c:if>>Year</option>
+		<option value="all" <c:if test="${'all' eq requestScope.etaparam}">selected</c:if>>All</option>
+	</select>
+</label>
+<label>
+nsfw:
+	<select name="nsfw">
+		<option value="yes"<c:if test="${'yes' eq requestScope.nsfwparam}">selected</c:if>>Yes .</option>
+		<option value="no" <c:if test="${'no' eq requestScope.nsfwparam}">selected</c:if>>No .</option>
+	</select>
+</label>
+</div>
 </form>
 
 <%-- Redirect back to index if no parameters are in --%>
@@ -13,7 +33,9 @@
 		<c:forEach items="${requestScope.submissions}" var="submission">
 			<a href="http://reddit.com/${submission.permalink}"><div class="pin">
 				<img src="${submission.thumbnail}"/>
-				<h6>from : ${submission.subreddit}</h6>
+				<jsp:useBean id="myDate" class="java.util.Date"/>  
+				<c:set target="${myDate}" property="time" value="${submission.createdUTC * 1000}"/>    
+				<h6>from : ${submission.subreddit} - <fmt:formatDate value="${myDate}"/></h6>
 				<h3>${submission.title}</h3>
 				<h4>score: ${submission.score}</h4>
 			</div></a>
